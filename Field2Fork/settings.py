619e1e306da21171012b27884d176b501fa97fbd
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-08-01 17:31:14 +0530

    Reverted Root urls

--- Field2Fork/settings.py
+++ Field2Fork/settings.py
@@ -135,9 +135,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/"static"
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/')
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
